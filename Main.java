ac89e6b038da55b3f7e4d9a9378b045bf23211d4
Katerina Walter <94501766+KaterinaWalter@users.noreply.github.com>
2025-11-10 14:39:06 +0000

    part a

--- Main.java
+++ Main.java
@@ -1,6 +1,22 @@
 public class Main {
 
    public static void main(String []args) {
-      System.out.println("Hello World");
-   }
-}
+      System.out.println("Welcome to Magic 8 Ball! 🔮 Think of a question...");
+
+      // Generate a random integer btwn 1-10
+      int num = (int) (Math.random() * 10 + 1); 
+      System.out.println(num);
+
+      // Conditional block to test the value of NUM
+      if ( num == 10 ) {
+         System.out.println("Doubtful...");
+      }
+      else if ( num == 9 ) {
+         System.out.println("Absolutely!!! NOT.");
+      }
+
+
+
+
+   } // close main method
+} // close class
